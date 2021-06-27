b1f1a7c1d367512227942d132c7a933aeea80470
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-27 22:45:43 +0200

    GlobalStyle created and added to index.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+
+${normalize}
+
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
+.body {
+    max-width: 900px;
+    margin: 0 auto;
+    font-size: 16px;
+    font-family: 'Jura', sans-serif;
+}
+`
