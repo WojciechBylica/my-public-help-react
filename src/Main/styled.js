2a814613930bbe1a79f5c2a5f22de072ab964859
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-28 21:09:59 +0200

    relocation of defining bg color

--- src/Main/styled.js
+++ src/Main/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledMain = styled.main`
     display: grid;
     grid-template-columns: auto 1fr;
+    background: #27a5e2ad;
 
     @media (max-width: 800px) {
         display: block;
