ecd8aab33b6eff463db8b441ff767f41d2ecb4e4
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-05 01:34:22 +0200

    formating

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,24 +1,20 @@
 import { ButtonsDiv, StyledButton } from "./styled";
 
-const Buttons = ({ helps, setHelpFromSource }) => {
-    return (
-        !!helps.length &&
-             <ButtonsDiv>
-                <>
-                    <StyledButton onClick={() => setHelpFromSource("ZUS")}>
-                        pokaż pomoc z ZUS
-                    </StyledButton>
-                    <StyledButton onClick={() => setHelpFromSource("UP")} >
-                        pokaż pomoc z UP
-                    </StyledButton>
-                    <StyledButton onClick={() => setHelpFromSource("all")}>
-                        pokaż wszystko
-                    </StyledButton>
-                </>
-            </ButtonsDiv>
-            
-    
-    );
-};
+const Buttons = ({ helps, setHelpFromSource }) => (
+    !!helps.length &&
+    <ButtonsDiv>
+        <>
+            <StyledButton onClick={() => setHelpFromSource("ZUS")}>
+                pokaż pomoc z ZUS
+            </StyledButton>
+            <StyledButton onClick={() => setHelpFromSource("UP")} >
+                pokaż pomoc z UP
+            </StyledButton>
+            <StyledButton onClick={() => setHelpFromSource("all")}>
+                pokaż wszystko
+            </StyledButton>
+        </>
+    </ButtonsDiv>
+);
 
 export default Buttons;
